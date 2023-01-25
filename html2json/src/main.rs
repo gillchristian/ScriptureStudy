090ede74895f86d7300ca97ce526009b5194789f
gillchristian <gillchristiang@gmail.com>
2023-01-25 08:54:01 +0100

    html2json: Error handling

--- html2json/src/main.rs
+++ html2json/src/main.rs
@@ -1,5 +1,6 @@
 use html_parser::Dom;
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::io;
 use std::io::Read;
@@ -12,30 +13,23 @@ fn fst_arg() -> Option<String> {
     fst.map(|p| p.to_string())
 }
 
-fn main() {
-    let html = match fst_arg().and_then(|path| fs::read_to_string(path).ok()) {
-        Some(html) => html,
-        None => {
-            let mut buf: Vec<u8> = vec![];
+fn read_stdin() -> Result<String, Box<dyn Error>> {
+    let mut buf: Vec<u8> = vec![];
 
-            io::stdin()
-                .read_to_end(&mut buf)
-                .ok()
-                .filter(|bytes_read| bytes_read > &0)
-                .and_then(|_| String::from_utf8(buf).ok())
-                .expect("Failed to read stdin")
-        }
+    io::stdin().read_to_end(&mut buf)?;
+
+    Ok(String::from_utf8(buf)?)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let html = match fst_arg() {
+        Some(path) => fs::read_to_string(path)?,
+        None => read_stdin()?,
     };
 
-    let dom = match Dom::parse(&html) {
-        Ok(dom) => dom,
-        Err(err) => panic!("{}", err),
-    };
-
-    let json = match dom.to_json() {
-        Ok(json) => json,
-        Err(err) => panic!("{}", err),
-    };
+    let json = Dom::parse(&html)?.to_json()?;
 
     println!("{}", json);
+
+    Ok(())
 }
