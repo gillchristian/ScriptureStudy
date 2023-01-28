78e08755b45288d130dfd4da61804b0197481d3c
gillchristian <gillchristiang@gmail.com>
2023-01-28 17:32:55 +0100

    app: Add home page

--- app/src/app/head.tsx
+++ app/src/app/head.tsx
@@ -4,10 +4,7 @@ export default function Head() {
       <title>Scripture Study</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="My personal bible study application" />
-      {
-        // TODO: add favicon
-        false && <link rel="icon" href="/favicon.ico" />
-      }
+      <link rel="icon" href="/favicon.ico" />
     </>
   )
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,15 +1,46 @@
 import Link from "next/link"
 
 import {CONFIG} from "@/config"
+import {Books, NamedReference, toChapters} from "@/models/reference"
 
-export default function Home() {
+export default async function Home() {
+  const version = CONFIG.DEFAULT_VERSION
+  const {books, chapters: _} = await getData(version)
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex flex-col">
-        <Link className="text-blue-600" href={`/${CONFIG.DEFAULT_VERSION}/genesis/1`}>
-          {CONFIG.DEFAULT_VERSION} Genesis 1
-        </Link>
+    <div className="flex h-screen w-screen justify-center p-4 pt-4 sm:pt-10">
+      <div className="container space-y-8">
+        <div>
+          <p className="m-2 border-b-2 border-gray-200 font-bold text-gray-600">Old Testament</p>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+            {books.inOrder.slice(0, 39).map((book) => (
+              <Link className="m-2 text-blue-600" href={`/${version}/${book}/1`} key={book}>
+                {books.names[book]}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="container">
+          <p className="m-2 border-b-2 border-gray-200 font-bold text-gray-600">New Testament</p>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+            {books.inOrder.slice(39).map((book) => (
+              <Link className="m-2 text-blue-600" href={`/${version}/${book}/1`} key={book}>
+                {books.names[book]}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
+}
+
+const getData = async (version: string): Promise<{books: Books; chapters: NamedReference[]}> => {
+  const url = `${CONFIG.API_URL}/chapters.json`
+
+  const books: Books = await fetch(url).then((res) =>
+    res.ok ? res.json() : Promise.reject(new Error("No chapters"))
+  )
+
+  return {books, chapters: toChapters(version, books)}
 }
