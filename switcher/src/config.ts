62cf5d0d5d84ec3337df287852d7804e30a3d0c0
gillchristian <gillchristiang@gmail.com>
2023-01-28 10:25:35 +0100

    app: Cleanup old switcher app

--- switcher/src/config.ts
+++
@@ -1,7 +0,0 @@
-export const CONFIG = {
-  API_URL: process.env.REACT_APP_API_URL ?? "http://localhost:3000",
-  DEFAULT_VERSION: process.env.REACT_APP_DEFAULT_VERSION ?? "WEB",
-  AVAILABLE_VERSIONS: (process.env.REACT_APP_AVAILABLE_VERSIONS ?? "WEB")
-    .split(",")
-    .sort()
-}
